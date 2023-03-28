964bdd03e6b1fb35e0338ab7a6ea66f2f0a2a88b
Exostose <bertrand.combes9@gmail.com>
2023-03-28 17:02:48 +0200

    Gros commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,85 +5,41 @@
  * @format
  */
 
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {
   SafeAreaView,
   useColorScheme,
-  Button as NButton,
-  Image,
-  Text,
-  View,
   StyleSheet,
+  ScrollView,
 } from 'react-native';
 
 import {Colors} from 'react-native/Libraries/NewAppScreen';
-import Button from './components/uis/Button/Button';
+import MainLayout from './components/layouts/MainLayout/MainLayout';
+import Menu from './components/uis/Menu/Menu';
+import ListProduct from './pages/ListProduct/ListProduct';
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
-
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.lighter : Colors.lighter,
   };
-  const [counter, setcounter] = useState(0);
-  useEffect(() => {
-    console.log('============================');
-    console.log('Nouvelle valeur de counter : ', counter);
-    console.log('============================');
-  }, [counter]);
-  useEffect(() => {
-    console.log('montage');
-    return () => {
-      console.log('demontage');
-    };
-  }, []);
+
   return (
     <SafeAreaView style={backgroundStyle}>
-      <View>
-        <Text style={styles.displayCounterText}>Valeur de mon compteur</Text>
-        <Text style={{...styles.displayCounterText, ...styles.big}}>
-          {counter}
-        </Text>
-      </View>
-      <Button
-        bgcolor="tomato"
-        color="green"
-        style={{borderRadius: 64}}
-        onPress={() => {
-          setcounter(counter - 1);
-        }}>
-        <Image
-          source={{
-            uri: 'https://cdn1.iconfinder.com/data/icons/city-flat-2/512/tree_element_nature_plant_garden_bush-1024.png',
-          }}
-          style={{width: 64, height: 64}}
-        />
-        <Text style={{...styles.displayCounterText, ...styles.big}}>
-          Soustraire
-        </Text>
-      </Button>
-      <Button
-        onPress={() => {
-          setcounter(counter + 1);
-        }}>
-        <Text style={{...styles.displayCounterText, ...styles.big}}>
-          Ajouter
-        </Text>
-      </Button>
-      {/* <Button children="Button3" /> */}
-      <NButton title="Hello World"></NButton>
+      <MainLayout>
+        <ScrollView style={styles.page}>
+          <ListProduct />
+        </ScrollView>
+        <Menu />
+        <ListProduct />
+      </MainLayout>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  displayCounterText: {
-    textAlign: 'center',
-    textAlignVertical: 'center',
-  },
-  big: {
-    fontSize: 18,
-    fontWeight: '900',
+  page: {
+    flexGrow: 1,
   },
 });
 
---
+++ src/components/uis/Menu/Menu.js
@@ -0,0 +1,41 @@
+import {View, Text, TouchableHighlight, Image} from 'react-native';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Menu.styles';
+/**
+ * initial value of menuState
+ */
+const menuInitialStateValue = '';
+/**
+ * Menu component
+ * @param {object} props Menu props of component
+ * @returns render of Menu component
+ */
+const Menu = props => {
+  return (
+    <View style={styles.Menu}>
+      <TouchableHighlight>
+        <Image
+          source={{
+            uri: 'https://cdn1.iconfinder.com/data/icons/real-estate-140/64/22_house_real_estate_architecture_home_fence-1024.png',
+          }}
+          style={styles.buttonImage}
+        />
+      </TouchableHighlight>
+      <TouchableHighlight>
+        <Image
+          source={{
+            uri: 'https://cdn3.iconfinder.com/data/icons/feather-5/24/arrow-right-circle-1024.png',
+          }}
+          style={styles.buttonImage}
+        />
+      </TouchableHighlight>
+      <Text>menu</Text>
+    </View>
+  );
+};
+
+Menu.propTypes = {};
+Menu.defaultProps = {};
+
+export default Menu;
