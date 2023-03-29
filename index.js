0f18ba82a54e3c331d931dd031972b9c6e6fb24a
Exostose <bertrand.combes9@gmail.com>
2023-03-29 12:37:38 +0200

    Création du provider des stores

--- index.js
+++ index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import React from 'react';
+import {Provider} from 'react-redux';
+import {store} from './src/store/store';
 
-AppRegistry.registerComponent(appName, () => App);
+const ProviderWrapper = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+};
+AppRegistry.registerComponent(appName, () => ProviderWrapper);
